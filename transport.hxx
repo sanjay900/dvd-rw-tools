637607fb98e7e1c6830b2e0ef56ce1fef065642b
Sanjay Govind <sanjay.govind9@gmail.com>
2025-10-25 16:50:34 +1300

    test

--- transport.hxx
+++ transport.hxx
@@ -1977,7 +1977,7 @@ public:
 		if(sub_dict == NULL)
 			return 0;
 		CFDictionarySetValue(sub_dict, CFSTR(kIOPropertySCSITaskDeviceCategory),
-							CFSTR("SCSITaskUserClientDevice"));
+							CFSTR("SCSITaskAuthoringDevice"));
 		CFDictionarySetValue(match_dict, CFSTR(kIOPropertyMatchKey), sub_dict);
 
 		printf("test2\r\n");
